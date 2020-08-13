88e6ed56e6a1c7dbca52540bf9eeba4e055d2dd5
Akhmerov <wertfani@mail.ru>
2020-08-13 13:33:41 +0700

    add-styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {App} from "./App";
+import './style/index.less';
 
 const root = document.getElementById('root');
 
--- src/modules/main/components/Page/index.jsx
+++ src/modules/main/components/Page/index.jsx
@@ -1,8 +1,6 @@
 import styles  from './style.local.less';
 import React from 'react';
 
-console.log(styles);
-
 export function Page() {
     return <div className={styles.page}>Page</div>
 }
