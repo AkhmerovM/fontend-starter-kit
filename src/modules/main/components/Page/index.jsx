977c9166f9aa82aefe3d72d2a8b20e69320f6c2c
Akhmerov <wertfani@mail.ru>
2020-08-18 17:46:31 +0700

    add-react-router-dom

--- src/modules/main/components/Page/index.jsx
+++ src/modules/main/components/Page/index.jsx
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import { ReactComponent as SmileIcon } from 'app/icons/smile.svg';
-import nature from 'app/images/nature.jpg';
+import nature from 'app/images/cat.jpg';
 import styles from './style.local.less';
 
 export function Page() {
@@ -9,7 +9,7 @@ export function Page() {
         <div className={styles.page}>
             <div className={styles.main}>Title page</div>
             <SmileIcon className={styles.icon} />
-            <img src={nature} alt="nature" />
+            <img className={styles.img} src={nature} alt="nature" />
         </div>
     );
 }
--- src/modules/main/containers/MainContainer/index.jsx
+++ src/modules/main/containers/MainContainer/index.jsx
@@ -3,9 +3,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMainUsers } from 'modules/main/selectors';
 import { Grid } from 'modules/main/components/Grid';
-import { Button } from 'react-bootstrap';
 import cn from 'classnames';
 import { getUsers } from 'modules/main/actions';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Page } from 'modules/main/components/Page';
 import styles from './style.local.less';
 
 export function MainContainer() {
@@ -17,10 +18,12 @@ export function MainContainer() {
     }, []);
     return (
         <div className={cn(styles.mainContainer, 'h_full')}>
-            <Grid />
-            <div>
-                <Button variant="primary">Primary</Button>
-            </div>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Grid} />
+                    <Route path="/main/edit" exact component={Page} />
+                </Switch>
+            </Router>
         </div>
     );
 }
