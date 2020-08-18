72e6d5e2eaa17541d16cb4542bd7057185ced58b
Akhmerov <wertfani@mail.ru>
2020-08-18 13:47:18 +0700

    grid

--- src/modules/main/components/Grid/index.jsx
+++ src/modules/main/components/Grid/index.jsx
@@ -4,12 +4,12 @@ import styles from './style.local.less';
 
 export function Grid() {
     return (
-        <div className="grid">
-            <div className="row with-wrap">
-                <div className="col-sm-5 col-md-4"><div className={cn(styles.block1)}>some text</div></div>
-                <div className="col-sm-5 col-md-6"><div className={cn(styles.block2)}>some text</div></div>
-                <div className="col-sm-3 col-md-6"><div className={cn(styles.block3)}>some text</div></div>
-            </div>
+        <div className={styles.grid}>
+            <div className={cn(styles.block1, styles.block)}>some text</div>
+            <div className={cn(styles.block2, styles.block)}>some text</div>
+            <div className={cn(styles.block3, styles.block)}>some text</div>
+            <div className={cn(styles.block4, styles.block)}>some text</div>
+            <div className={cn(styles.block5, styles.block)}>some text</div>
         </div>
     );
 }
--- src/modules/main/containers/MainContainer/index.jsx
+++ src/modules/main/containers/MainContainer/index.jsx
@@ -1,16 +1,12 @@
 // @flow
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectMainUsers } from 'modules/main/selectors';
+import { useDispatch } from 'react-redux';
 import { Grid } from 'modules/main/components/Grid';
-import { Button } from 'react-bootstrap';
 import cn from 'classnames';
 import { getUsers } from 'modules/main/actions';
 import styles from './style.local.less';
 
 export function MainContainer() {
-    const users = useSelector(selectMainUsers);
-    console.log(users);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getUsers());
@@ -18,9 +14,6 @@ export function MainContainer() {
     return (
         <div className={cn(styles.mainContainer, 'h_full')}>
             <Grid />
-            <div>
-                <Button variant="primary">Primary</Button>
-            </div>
         </div>
     );
 }
